6a08503d93ef7cee7dfa74813d876d1a88380094
pc-1827 <problemcreator1827@gmail.com>
2024-02-21 22:56:23 +0530

    file restrcuture

--- popup.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const recordButton = document.getElementById('recordButton');
-
-    recordButton.addEventListener('click', function() {
-      // Send a message to the background script to start audio capture
-      chrome.runtime.sendMessage('recordAudio');
-      console.log("Hello there, form Audio Recorder")
-    });
-  });
